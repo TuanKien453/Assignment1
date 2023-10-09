1fc0f29b52519dfaef5d6e1c4aeaaed475ff4d70
TuanKien <TuanKien0408@gmail.com>
2023-10-09 16:55:53 +0700

    note

--- Assignment 1/src/classes/BrandList.java
+++ Assignment 1/src/classes/BrandList.java
@@ -7,6 +7,13 @@ public class BrandList {
     public BrandList() {
     }
     
+//    public boolean loadFromFile(String);
+//    public boolean saveToFile(String);
+//    public int searchID (String ID);
+//    public Brand getUserChoice();
+//    public void addBrand();
+//    public void updateBrand();
+//    public void listBrands();
     
     
 }
