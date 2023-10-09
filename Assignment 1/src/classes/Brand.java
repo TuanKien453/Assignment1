5d0c62c399b7d497160bd9311f843990dcc9fe18
TuanKien <TuanKien0408@gmail.com>
2023-10-09 16:07:27 +0700

    Brand class

---
+++ Assignment 1/src/classes/Brand.java
@@ -0,0 +1,59 @@
+package classes;
+
+
+public class Brand {
+    private String brandID;
+    private String brandName;
+    private String soundBrand;
+    private double price;
+
+    public Brand(String brandID, String brandName, String soundBrand, double price) {
+        this.brandID = brandID;
+        this.brandName = brandName;
+        this.soundBrand = soundBrand;
+        this.price = price;
+    }
+
+    public Brand() {
+    }
+
+    public String getBrandID() {
+        return brandID;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public String getSoundBrand() {
+        return soundBrand;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setBrandID(String brandID) {
+        this.brandID = brandID;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public void setSoundBrand(String soundBrand) {
+        this.soundBrand = soundBrand;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        String s = getClass().getSimpleName();
+        return s + "brandID=" + brandID + ", brandName=" + brandName + ", soundBrand=" + soundBrand + ", price=" + price;
+    }
+    
+    
+}
---
+++ Assignment 1/src/classes/BrandList.java
@@ -0,0 +1,12 @@
+package classes;
+import java.util.*;
+
+public class BrandList {
+    private Brand[] brandList ;
+
+    public BrandList() {
+    }
+    
+    
+    
+}
