6b39b08d71a5617bb3e19ce9a498b975a99e22da
TuanKien <TuanKien0408@gmail.com>
2023-10-10 12:46:08 +0700

    add some method in BrandList

--- Assignment 1/src/classes/BrandList.java
+++ Assignment 1/src/classes/BrandList.java
@@ -2,9 +2,10 @@ package classes;
 import java.util.*;
 
 public class BrandList {
-    private Brand[] brandList ;
+    private ArrayList<Brand> brandList;
 
     public BrandList() {
+        brandList = new ArrayList<Brand>();
     }
     
 //    public boolean loadFromFile(String);
@@ -14,6 +15,34 @@ public class BrandList {
 //    public void addBrand();
 //    public void updateBrand();
 //    public void listBrands();
-    
+    public boolean searchID(String ID) {
+        for (int i = 0; i < brandList.size(); i++) {
+            if (brandList.get(i).getBrandID().equals(ID)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addBrand() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter brand ID: ");
+        String brandID = scanner.nextLine();
+        boolean index = searchID(brandID);
+        if (!index) {
+            System.out.print("Enter brand name: ");
+            String brandName = scanner.nextLine();
+            System.out.print("Enter sound brand: ");
+            String soundBrand = scanner.nextLine();
+            System.out.print("Enter price: ");
+            double price = scanner.nextDouble();
+            scanner.nextLine(); // Clear the newline character from the input buffer
+            Brand brand = new Brand(brandID, brandName, soundBrand, price);
+            brandList.add(brand);
+            System.out.println("Brand added successfully!");
+        } else {
+            System.out.println("Brand with the same ID already exists!");
+        }
+    }
     
 }
